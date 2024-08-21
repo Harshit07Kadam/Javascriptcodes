5a9a59fc4a739951d1f8d4cafa2eca6f8994dc98
Harshit07Kadam <kadamharshit2004@gmail.com>
2024-08-21 20:24:21 +0530

    anoynmus.js

--- Functions/anoynmus.js
+++ Functions/anoynmus.js
@@ -1,4 +1,8 @@
 const prompt = require('prompt-sync')();
+let show = function (P,R,N) {
+    console.log ("THE SIMPLE INTEREST BY ANONYMOUS FUNCTION IS = " + (P*R*N)/100)
+};
 let P = prompt ("ENTER THE PRINCIPAL=");
 let R = prompt ("ENTER THE RATE OF INTEREST");
 let N = prompt ("ENTER THE YEARS= ");
+show(P,R,N);
