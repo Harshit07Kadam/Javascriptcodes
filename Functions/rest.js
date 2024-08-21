a0fe908b88a34606ae42f061844c36dd6d5e93f9
Harshit07Kadam <kadamharshit2004@gmail.com>
2024-08-21 20:25:14 +0530

    rest.js

--- Functions/rest.js
+++ Functions/rest.js
@@ -1,4 +1,10 @@
 const prompt = require('prompt-sync')();
+function simple (...theArgs) {
+    let [P,R,N] = theArgs;
+    let A = (P*R*N)/100;
+    console.log("THE SIMPLE INTEREST BY REST PARAMETER FUNCTION IS ="+A )
+}
 let P = prompt ("ENTER THE PRINCIPAL=");
 let R = prompt ("ENTER THE RATE OF INTEREST");
 let N = prompt ("ENTER THE YEARS= ");
+simple(P,R,N);
